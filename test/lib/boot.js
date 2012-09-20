2ec0478418d7e8e93c5d779f341bf6a2654413d1
Nick Payne <nick@kurai.co.uk>
2012-09-20 14:51:26 +0100

    remove hard coded fixture file path

--- test/lib/boot.js
+++ test/lib/boot.js
@@ -4,7 +4,7 @@ var child_process = require("child_process"),
 Settings.setModeAndLoad(process.env.PROJECT_MODE || "test");
 
 module.exports = {
-    loadFixture: function(cb) {
+    loadFixture: function(fixture, cb) {
 
         var sql = 
         "mysql -u"+Settings.getValue("db", "user")+" "+
@@ -12,7 +12,7 @@ module.exports = {
         "-p"+Settings.getValue("db", "pass")+" "+
         "--database="+Settings.getValue("db", "dbname")+" "+
         "--port="+Settings.getValue("db", "port", "3306")+" "+
-        "< "+__dirname+"/../../tests/fixtures/salon_populated.sql";
+        "< "+__dirname+"/../../tests/fixtures/"+fixture+".sql";
 
         child_process.exec(sql, cb);
     },
